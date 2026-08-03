04973acf80df28e0b3b59f795b65ddfd1a8a64c1
Sskumar05 <sshathiskumar54@gmail.com>
2026-08-03 16:07:22 +0530

    Commit

--- supabase/functions/send-email/index.ts
+++ supabase/functions/send-email/index.ts
@@ -136,12 +136,12 @@ ${preheader ? `<div style="display:none;max-height:0;overflow:hidden;color:trans
     <div class="header" style="background-color:#1a1510 !important;">
    <table role="presentation" width="100%" cellpadding="0" cellspacing="0" border="0">
   <tr>
-    <td align="center" style="padding-bottom:20px;">
+    <td align="center" style="padding-bottom:10px;">
       <img
         src="https://res.cloudinary.com/dhjupdyus/image/upload/v1785752258/Emirates_logopng_qpavp4.png"
         alt="Emirates Group"
-        width="80"
-        height="80"
+        width="90"
+        height="90"
         style="display:block;border:0;outline:none;text-decoration:none;width:80px;height:80px;"
       />
     </td>
